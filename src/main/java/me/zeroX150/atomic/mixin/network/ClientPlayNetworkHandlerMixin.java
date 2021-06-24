c2b1fa8300923e1102cddad9171f53bb32ae8bb3
MisterX <weinboy11@gmail.com>
2021-06-24 16:08:23 +0200

    dimension detection

--- src/main/java/me/zeroX150/atomic/mixin/network/ClientPlayNetworkHandlerMixin.java
+++ src/main/java/me/zeroX150/atomic/mixin/network/ClientPlayNetworkHandlerMixin.java
@@ -4,6 +4,8 @@ import me.zeroX150.atomic.feature.module.ModuleRegistry;
 import me.zeroX150.atomic.feature.module.impl.render.OreSim;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.network.packet.s2c.play.ChunkDataS2CPacket;
+import net.minecraft.network.packet.s2c.play.GameJoinS2CPacket;
+import net.minecraft.network.packet.s2c.play.PlayerRespawnS2CPacket;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -19,6 +21,24 @@ public class ClientPlayNetworkHandlerMixin {
             oresim = (OreSim) ModuleRegistry.getByClass(OreSim.class);
         if (oresim.isEnabled()) {
             oresim.doMathOnChunk(packet.getX(), packet.getZ());
+        }
+    }
+
+    @Inject(method = "onGameJoin", at = @At(value = "TAIL"))
+    private void onGameJoin(GameJoinS2CPacket packet, CallbackInfo ci) {
+        reloadOresim();
+    }
+
+    @Inject(method = "onPlayerRespawn", at = @At("TAIL"))
+    private void respawn(PlayerRespawnS2CPacket packet, CallbackInfo ci) {
+        reloadOresim();
+    }
+
+    private void reloadOresim() {
+        if (oresim == null)
+            oresim = (OreSim) ModuleRegistry.getByClass(OreSim.class);
+        if (oresim.isEnabled()) {
+            oresim.reload();
         }
     }
 }
