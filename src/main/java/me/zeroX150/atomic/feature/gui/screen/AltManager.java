007ef71f86d1d3544f0054ab470b30eb83dbbecd
0x150 <constantindiscord@gmail.com>
2021-06-26 19:45:59 +0200

    the j

--- src/main/java/me/zeroX150/atomic/feature/gui/screen/AltManager.java
+++ src/main/java/me/zeroX150/atomic/feature/gui/screen/AltManager.java
@@ -3,7 +3,9 @@ package me.zeroX150.atomic.feature.gui.screen;
 import me.zeroX150.atomic.Atomic;
 import me.zeroX150.atomic.feature.module.impl.external.Alts;
 import me.zeroX150.atomic.helper.Client;
+import net.minecraft.client.gui.Element;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.ingame.CommandBlockScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.gui.widget.TextFieldWidget;
 import net.minecraft.client.util.math.MatrixStack;
@@ -25,10 +27,33 @@ public class AltManager extends Screen {
     @Override
     protected void init() {
         super.init();
+        this.client.keyboard.setRepeatEvents(true);
         username = new TextFieldWidget(Atomic.client.textRenderer, width / 2 - (200 / 2), height / 2 - (20 / 2) - 22, 200, 20, Text.of("SPECIAL:Username"));
         username.setMaxLength(65535);
         password = new TextFieldWidget(Atomic.client.textRenderer, width / 2 - (200 / 2), height / 2 - (20 / 2), 200, 20, Text.of("SPECIAL:Password"));
         password.setMaxLength(65535);
+        username.setChangedListener(v -> {
+            String s = username.getText();
+            if (s.contains(":")) {
+                String[] pair = s.split(":");
+                if (pair.length < 2) return;
+                String uname = pair[0];
+                String pw = pair[1];
+                username.setText(uname);
+                password.setText(pw);
+            }
+        });
+        password.setChangedListener(v -> {
+            String s = password.getText();
+            if (s.contains(":")) {
+                String[] pair = s.split(":");
+                if (pair.length < 2) return;
+                String uname = pair[0];
+                String pw = pair[1];
+                username.setText(uname);
+                password.setText(pw);
+            }
+        });
         login = new ButtonWidget(width / 2 - (150 / 2), height / 2 - (20 / 2) + 22, 150, 20, Text.of("Login"), button -> {
             boolean done = Client.auth(username.getText(), password.getText());
             feedback = done ? "§aLogged in!" : "§cFailed to login!";
@@ -79,6 +104,7 @@ public class AltManager extends Screen {
 
     @Override
     public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
+        this.client.keyboard.setRepeatEvents(true);
         renderBackground(matrices);
 
         drawCenteredText(matrices, textRenderer, feedback, width / 2, 50, 0xFFFFFF);
