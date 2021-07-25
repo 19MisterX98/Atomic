4b5eda04f869b3061c13f9c7f46dd85d1daceb2a
0x150 <constantindiscord@gmail.com>
2021-07-26 00:12:11 +0200

    why

--- src/main/java/me/zeroX150/atomic/feature/gui/clickgui/KeyListenerBtn.java
+++ src/main/java/me/zeroX150/atomic/feature/gui/clickgui/KeyListenerBtn.java
@@ -3,14 +3,21 @@ package me.zeroX150.atomic.feature.gui.clickgui;
 import me.zeroX150.atomic.Atomic;
 import me.zeroX150.atomic.feature.module.Module;
 import me.zeroX150.atomic.helper.keybind.KeybindManager;
+import net.minecraft.SharedConstants;
+import net.minecraft.client.gui.screen.option.ControlsOptionsScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.Text;
+import org.lwjgl.glfw.GLFW;
 
 public class KeyListenerBtn extends ButtonWidget {
     Module parent;
-    boolean listening = false;
+    public boolean listening = false;
     int kc;
+    int sc;
+    public long stoppedScanning = 0;
 
     public KeyListenerBtn(int x, int y, int width, Module parent) {
         super(x, y, width, 12, Text.of(String.valueOf((char) Integer.parseInt(parent.config.get("Keybind").getValue() + "")).toUpperCase()), button -> {
@@ -24,31 +31,30 @@ public class KeyListenerBtn extends ButtonWidget {
         listening = true;
     }
 
-    // this code is incredibly chinese
     @Override
-    public boolean charTyped(char chr, int modifiers) {
+    public boolean keyPressed(int keyCode, int scanCode, int modifiers) {
         if (!this.listening) return false;
-        String v = chr + "";
-        if ((int) chr == 47 || chr == '-') {
+        stoppedScanning = System.currentTimeMillis();
+        if (keyCode == 47 || ((char) keyCode) == '-') {
             listening = false;
             kc = -1;
             parent.config.get("Keybind").setValue(kc);
             KeybindManager.reload();
             return true;
         }
-        v = v.toUpperCase();
-        kc = v.charAt(0);
+        kc = keyCode;
+        sc = scanCode;
         listening = false;
         parent.config.get("Keybind").setValue(kc);
         KeybindManager.reload();
-        this.setMessage(Text.of(String.valueOf(chr)));
-        return true;
+        //this.setMessage(InputUtil.fromKeyCode(keyCode, scanCode).getLocalizedText());
+        return super.keyPressed(keyCode, scanCode, modifiers);
     }
 
     @Override
     public void renderButton(MatrixStack matrices, int mouseX, int mouseY, float delta) {
         if (listening) this.setMessage(Text.of("... (- to cancel)"));
-        else this.setMessage(Text.of(kc != -1 ? String.valueOf((char) kc) : "None"));
+        else this.setMessage(kc != -1 ? Text.of(String.valueOf((char) kc)) : Text.of("None"));
         fill(matrices, x, y, x + width, y + height, (this.isHovered() ? ClickGUI.currentActiveTheme.active() : ClickGUI.currentActiveTheme.inactive()).getRGB());
         Atomic.fontRenderer.drawCenteredString(matrices, this.getMessage().asString(), x + width / 2f, y + height / 2f - 8 / 2f, ClickGUI.currentActiveTheme.fontColor().getRGB());
         //DrawableHelper.drawCenteredText(matrices, Atomic.client.textRenderer, this.getMessage(), x + (width / 2), y + (height / 2 - 9 / 2), 0xFFFFFF);
